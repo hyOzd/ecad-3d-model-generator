f480eb5cbc98bd05fe88829df34f617874e1e198
Hasan Yavuz ÖZDERYA <hy@ozderya.net>
2015-08-26 00:00:28 +0300

    added DIP components to database

--- e3dmg/database/dip.py
+++ e3dmg/database/dip.py
@@ -18,15 +18,25 @@
 # along with ecad-3d-model-generator.  If not, see
 # <http://www.gnu.org/licenses/>.
 
-from e3dmg.generators.dip import DIPGen, DIP300Gen, DIP600Gen
+from e3dmg.generators.dip import DIP300Gen, DIP600Gen
 from e3dmg.utils import mm
 
 DIP08 = DIP300Gen(
     D = 9.27,   # package length
     npins = 8  # total number of pins
 )
-
-DIP22_6 = DIP600Gen(
-    D = mm(1.1),
-    npins = 22
-)
+DIP06 = DIP300Gen(7.05, 6)
+DIP14 = DIP300Gen(19.05, 14)
+DIP16 = DIP300Gen(mm(0.755), 16)
+DIP18 = DIP300Gen(mm(0.9), 18)
+DIP20 = DIP300Gen(mm(1.03), 20)
+DIP22 = DIP300Gen(mm(1.1), 22)
+DIP24 = DIP300Gen(mm(1.25), 24)
+DIP28 = DIP300Gen(mm(1.4), 28)
+DIP22_6 = DIP600Gen(mm(1.1), 22)
+DIP24_6 = DIP600Gen(mm(1.25), 24)
+DIP28_6 = DIP600Gen(mm(1.4), 28)
+DIP32_6 = DIP600Gen(mm(1.63), 32)
+DIP40_6 = DIP600Gen(mm(2), 40)
+DIP48_6 = DIP600Gen(mm(2.42), 48)
+DIP52_6 = DIP600Gen(mm(2.6), 52)
