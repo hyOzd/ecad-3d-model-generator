37ffa88f1661ccc656812b45df87d0017e5218aa
Hasan Yavuz ÖZDERYA <hy@ozderya.net>
2016-02-28 14:31:14 +0200

    added experimental VRML exporter

--- e3dmg/exporters/export_vrml.py
+++ e3dmg/exporters/export_vrml.py
@@ -34,6 +34,44 @@
 
 from e3dmg import Material
 
+def meshToVRML(mesh):
+    """Returns the VRML Shape node representation of a `Mesh`"""
+    s = "Shape { geometry IndexedFaceSet { coordIndex ["
+    # write coordinate indexes for each face
+    s += ','.join("%d,%d,%d,-1" % f for f in mesh.faces)
+    s += "]" # closes coordIndex
+    s += "coord Coordinate { point ["
+    # write coordinate points for each vertex
+    s += ','.join('%.3f %.3f %.3f' % (p.x, p.y, p.z) for p in mesh.points)
+    s += "]}" # closes Coordinate
+    s += "}\n" # closes IndexedFaceSet
+
+    if isinstance(mesh.color, Material):
+        material = "diffuseColor %f %f %f\n" % mesh.color.diffuseColor + \
+                   "ambientIntensity %f\n" % mesh.color.ambientIntensity + \
+                   "specularColor %f %f %f\n" % mesh.color.specularColor + \
+                   "shininess %f\n" % mesh.color.shininess + \
+                   "emissiveColor %f %f %f\n" % mesh.color.emissiveColor + \
+                   "transparency %f\n" % mesh.color.transparency
+        s += "appearance Appearance{material Material{%s}}" % material
+    else:
+        print(mesh.color)
+        s += "appearance Appearance{material Material{diffuseColor %f %f %f}}" % mesh.color
+
+    s += "}\n" # closes Shape
+
+    return s
+
+def shapeToMesh(shape, color, scale=None):
+    from export_x3d import Mesh
+    mesh_data = shape.tessellate(1)
+    points = mesh_data[0]
+    if scale != None:
+        points = map(lambda p: p*scale, points)
+    return Mesh(points = points,
+                faces = mesh_data[1],
+                color = color)
+
 def exportVRML(objects, filepath):
     """Export given list of Mesh objects to a VRML file.
 
@@ -44,32 +82,23 @@ def exportVRML(objects, filepath):
         f.write("#VRML V2.0 utf8\n\n")
 
         for obj in objects:
-            f.write("Shape { geometry IndexedFaceSet { coordIndex [")
+            f.write(meshToVRML(obj))
 
-            # write coordinate indexes for each face
-            f.write(','.join("%d,%d,%d,-1" % f for f in obj.faces))
+def exportVRML2(objects, filepath):
+    """This is an EXPERIMENTAL exporter. Unlike above one, this one can
+    export fused multi color FreeCAD shapes, but it also relies on
+    FreeCADGui module. Output isn't good either (each face exported as
+    a different shape).
+    """
 
-            f.write("]") # closes coordIndex
+    with open(filepath, 'w') as f:
+        # write the standard VRML header
+        f.write("#VRML V2.0 utf8\n\n")
 
-            f.write("coord Coordinate { point [")
-
-            # write coordinate points for each vertex
-            f.write(','.join('%.3f %.3f %.3f' % (p.x, p.y, p.z) for p in obj.points))
-
-            f.write("]}") # closes Coordinate
-
-            f.write("}\n") # closes IndexedFaceSet
-
-            # TODO: materials
-            if isinstance(obj.color, Material):
-                material = "diffuseColor %f %f %f\n" % obj.color.diffuseColor + \
-                           "ambientIntensity %f\n" % obj.color.ambientIntensity + \
-                           "specularColor %f %f %f\n" % obj.color.specularColor + \
-                           "shininess %f\n" % obj.color.shininess + \
-                           "emissiveColor %f %f %f\n" % obj.color.emissiveColor + \
-                           "transparency %f\n" % obj.color.transparency
-                f.write("appearance Appearance{material Material{%s}}" % material)
+        for obj in objects:
+            # does object have different colored faces?
+            if len(obj.ViewObject.DiffuseColor) > 1:
+                for i, face in enumerate(obj.Shape.Faces):
+                    f.write(meshToVRML(shapeToMesh(face, obj.ViewObject.DiffuseColor[i][0:3])))
             else:
-                f.write("appearance Appearance{material Material{diffuseColor %f %f %f}}" % obj.color)
-
-            f.write("}\n") # closes Shape
+                raise NotImplemented("Use other exporter :P")
